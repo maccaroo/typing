dc767494034e0c077cd3f9ea94c5375552925930
Mac Krzysik <mac@gamanoju.com>
2023-01-11 07:00:19 +0800

    full

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faker } from "@faker-js/faker";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.lorem.word();
+  enteredText = "";
+  isSolved = false;
+
+  onInput(event: any) {
+    this.enteredText = event.target.value;
+    this.isSolved = this.enteredText === this.randomText;
+  }
+
+  getClass(letter: string, enteredTextAtI: string) {
+    if (!enteredTextAtI)
+      return "pendingText";
+    else if (letter === enteredTextAtI)
+      return "correctText";
+    else
+      return "errorText";
+  }
 }
